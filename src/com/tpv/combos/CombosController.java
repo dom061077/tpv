9efb62b8dd839629e95abc635fb88a71d740f8ac
dom061077 <dom061077@gmail.com>
2016-05-24 12:28:11 -0300

    Fix para el manejo de transacciones

--- src/com/tpv/combos/CombosController.java
+++ src/com/tpv/combos/CombosController.java
@@ -10,7 +10,10 @@ import com.tpv.exceptions.TpvException;
 import com.tpv.modelo.Factura;
 import com.tpv.modelo.FacturaDetalleCombo;
 import com.tpv.principal.DataModelTicket;
+import com.tpv.principal.LineaTicketData;
 import com.tpv.service.FacturacionService;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.Iterator;
 import javafx.application.Platform;
 import javafx.beans.property.ListProperty;
@@ -19,7 +22,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -41,29 +48,112 @@ public class CombosController{
     @FXML
     private TableView tableViewCombos;
     
+    @FXML
+    private TableColumn codigoColumn;
+    
+    @FXML
+    private TableColumn descripcionColumn;
+    
+    @FXML
+    private TableColumn cantidadColumn;
+    
+    @FXML
+    private TableColumn subTotalColumn;
+    
+    @FXML
+    private Button goToErrorButton;
+    
+    @FXML
+    private Label totalTicket;
+    
+    @FXML
+    private Label totalBonificaciones;
+    
+    @FXML
+    private Label terminaPagando;
+    
+    private BigDecimal totalBonificado;
+    
+    
+           
+    
     @Inject
     private DataModelTicket modelTicket;
-    
-    
     private FacturacionService factService = new FacturacionService();
+    
+    ObservableList<FacturaDetalleComboData> combosItems = FXCollections.observableArrayList();
+    ListProperty<FacturaDetalleComboData> listCombos = new SimpleListProperty<>(combosItems);
+    
     
     @PostConstruct
     public void init(){
         Platform.runLater(() -> {
+            initTableViewCombos();
             tableViewCombos.setOnKeyPressed(keyEvent->{
                 if(keyEvent.getCode()==KeyCode.ESCAPE){
                     volverButton.fire();
                 }
             });
+            traerCombos();
+            tableViewCombos.setItems(listCombos);
+            
         });      
     }
     
-    private void verCombos(){
-        ObservableList<FacturaDetalleComboData> combosItems = FXCollections.observableArrayList();
-        ListProperty<FacturaDetalleComboData> listCombos = new SimpleListProperty<>(combosItems);
+    
+    private void initTableViewCombos(){
+        codigoColumn.setCellValueFactory(new PropertyValueFactory<LineaTicketData,Integer>("codigoProducto"));
+        codigoColumn.setStyle("-fx-alignment: CENTER-RIGHT;");
+        descripcionColumn.setCellValueFactory(new PropertyValueFactory("descripcion"));
+        cantidadColumn.setCellValueFactory(new PropertyValueFactory("cantidad"));
+        cantidadColumn.setStyle("-fx-alignment: CENTER-RIGHT;");
+        
+        subTotalColumn.setCellValueFactory(new PropertyValueFactory("subTotal"));
+        subTotalColumn.setCellFactory(col->{
+            TableCell<LineaTicketData,BigDecimal> cell = new TableCell<LineaTicketData,BigDecimal>(){
+                @Override
+                public void updateItem(BigDecimal item, boolean empty){
+                    super.updateItem(item, empty);
+                    this.setText(null);
+                    this.setGraphic(null);
+                    if (!empty) {
+                            //String formattedDob = De
+                            DecimalFormat df = new DecimalFormat("#,###,###,##0.00");
+                                    
+                            this.setText(df.format(item));
+                    }
+                }
+            };
+            return cell;
+        });
+        subTotalColumn.setStyle("-fx-alignment: CENTER-RIGHT;");
+        
+        subTotalColumn.setCellFactory(col -> {
+            TableCell<LineaTicketData,BigDecimal>cell = new TableCell<LineaTicketData,BigDecimal>(){
+                @Override
+                public void updateItem(BigDecimal item,boolean empty){
+                    super.updateItem(item, empty);
+                    this.setText(null);
+                    this.setGraphic(null);
+                    if (!empty) {
+                            //String formattedDob = De
+                            DecimalFormat df = new DecimalFormat("#,###,###,##0.00");
+                                    
+                            this.setText(df.format(item));
+                    }
+                }
+            };
+            return cell;
+        });
+        
+    }
+    
+    
+    private void traerCombos(){
         
         try{
             Factura factura = factService.calcularCombos(modelTicket.getIdFactura());
+            totalBonificado = BigDecimal.ZERO;
             for(Iterator<FacturaDetalleCombo> it = factura.getDetalleCombosAux().iterator();it.hasNext();){
                 FacturaDetalleCombo fdc = it.next();
                 FacturaDetalleComboData fdcd = new FacturaDetalleComboData(
@@ -72,7 +162,12 @@ public class CombosController{
                             fdc.getCantidad(),
                             fdc.getBonificacion()
                         );
+                totalBonificado = totalBonificado.add(fdc.getBonificacion());
                 listCombos.add(fdcd);
+                DecimalFormat df = new DecimalFormat("#,###,###,##0.00");
+                totalBonificaciones.setText(df.format(totalBonificado));
+                totalTicket.setText(df.format(modelTicket.getTotalTicket()));
+                terminaPagando.setText(df.format(modelTicket.getTotalTicket().subtract(totalBonificado)));
                 
             }
         }catch(TpvException e){
